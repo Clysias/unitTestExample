907bd86ee83966105cfdb885b08d71660e6eba6a
Claudio Lysias <clysias@facebook.com>
2017-12-11 15:48:17 -0200

    Initial Commit

---
+++ unitTestExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  unitTestExample
+//
+//  Created by claudio lysias on 11/12/2017.
+//  Copyright © 2017 claudio lysias aplicativos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
